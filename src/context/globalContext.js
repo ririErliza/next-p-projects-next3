2e00a8239ed101a1d3a5096e68d8fc7839a52134
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-08-11 11:56:10 +0200

    update

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -1,8 +1,9 @@
 "use client";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
+  const [showNavModal, setShowNavModal] = useState(false);
   return <GlobalContext.Provider calue={{}}>{children}</GlobalContext.Provider>;
 }
