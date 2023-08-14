30cbd43dd8002f15bd5fbac68acc877bf8d22a11
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-08-14 11:01:06 +0200

    update

---
+++ src/utils/registrationFormControls.js
@@ -0,0 +1,40 @@
+export const registrationFormControls = [
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter your name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+  // {
+  //   id: "role",
+  //   type: "",
+  //   placeholder: "",
+  //   label: "Role",
+  //   componentType: "select",
+  //   options: [
+  //     {
+  //       id: "admin",
+  //       label: "Admin",
+  //     },
+  //     {
+  //       id: "customer",
+  //       label: "customer",
+  //     },
+  //   ],
+  // },
+];
