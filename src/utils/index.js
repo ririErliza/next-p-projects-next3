de861641d57457217a6449be9d3e9bec6b6e6c46
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-08-16 11:52:31 +0200

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -96,3 +96,144 @@ export const loginFormControls = [
     componentType: "input",
   },
 ];
+
+export const adminAddProductformControls = [
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "price",
+    type: "number",
+    placeholder: "Enter price",
+    label: "Price",
+    componentType: "input",
+  },
+  {
+    id: "description",
+    type: "text",
+    placeholder: "Enter description",
+    label: "Description",
+    componentType: "input",
+  },
+  {
+    id: "category",
+    type: "",
+    placeholder: "",
+    label: "Category",
+    componentType: "select",
+    options: [
+      {
+        id: "men",
+        label: "Men",
+      },
+      {
+        id: "women",
+        label: "Women",
+      },
+      {
+        id: "kids",
+        label: "Kids",
+      },
+    ],
+  },
+  {
+    id: "deliveryInfo",
+    type: "text",
+    placeholder: "Enter deliveryInfo",
+    label: "Delivery Info",
+    componentType: "input",
+  },
+  {
+    id: "onSale",
+    type: "",
+    placeholder: "",
+    label: "On Sale",
+    componentType: "select",
+    options: [
+      {
+        id: "yes",
+        label: "Yes",
+      },
+      {
+        id: "no",
+        label: "No",
+      },
+    ],
+  },
+  {
+    id: "priceDrop",
+    type: "number",
+    placeholder: "Enter Price Drop",
+    label: "Price Drop",
+    componentType: "input",
+  },
+];
+
+export const AvailableSizes = [
+  {
+    id: "s",
+    label: "S",
+  },
+  {
+    id: "m",
+    label: "M",
+  },
+  {
+    id: "l",
+    label: "L",
+  },
+];
+
+export const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STROAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "MEASUREMENT_ID",
+};
+
+export const firebaseStroageURL = "YOUR_FIREBASE_STROAGE_URL";
+
+export const addNewAddressFormControls = [
+  {
+    id: "fullName",
+    type: "input",
+    placeholder: "Enter your full name",
+    label: "Full Name",
+    componentType: "input",
+  },
+  {
+    id: "address",
+    type: "input",
+    placeholder: "Enter your full address",
+    label: "Address",
+    componentType: "input",
+  },
+  {
+    id: "city",
+    type: "input",
+    placeholder: "Enter your city",
+    label: "City",
+    componentType: "input",
+  },
+  {
+    id: "country",
+    type: "input",
+    placeholder: "Enter your country",
+    label: "Country",
+    componentType: "input",
+  },
+  {
+    id: "postalCode",
+    type: "input",
+    placeholder: "Enter your postal code",
+    label: "Postal Code",
+    componentType: "input",
+  },
+];
